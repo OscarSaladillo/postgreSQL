73a0332d2588462ac50234f3019a50a79a28fa48
OscarSaladillo <91348351+OscarSaladillo@users.noreply.github.com>
2021-10-04 14:28:05 +0200

    jaks

---
+++ pg_env.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+REM The script sets environment variables helpful for PostgreSQL
+
+@SET PATH="C:\Program Files\Odoo 14.0.20210930\PostgreSQL\bin";%PATH%
+@SET PGDATA=C:\Program Files\Odoo 14.0.20210930\PostgreSQL\data
+@SET PGDATABASE=postgres
+@SET PGUSER=openpg
+@SET PGPORT=5432
+@SET PGLOCALEDIR=C:\Program Files\Odoo 14.0.20210930\PostgreSQL\share\locale
+
+                            
